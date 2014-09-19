280c26082bf64499cad5f63c7885fef84f27ba8d
Marcin Michalski <michalskimp@gmail.com>
2014-09-19 18:01:44 +0200

    Zadanie 3

--- scripts/gaslib/Iterator.js
+++ scripts/gaslib/Iterator.js
@@ -3,11 +3,20 @@ define([], function(){
 
     var Iterator = function(collection){
         this.collection = collection;
-        this.position = -1;
+        this.position = 0;
     };
 
-
     Iterator.constructor = Iterator;
+
+    Iterator.prototype.getNext = function()
+    {
+        return this.collection[this.position++];
+    }
+
+    Iterator.prototype.end = function()
+    {
+        return this.position == this.collection.length;
+    }
 
 
     return Iterator;
---
+++ testorial/tests/A beginner/1 getting started/Collection/Iterator/script.js
@@ -0,0 +1,24 @@
+require([
+    "gaslib/Collection",
+    "gaslib/Iterator"
+], function(Collection, Iterator){
+
+    QUnit.test( "Iterate throught elements", function( assert ) {
+
+        var collection = new Collection();
+        collection.add(6);
+        collection.add("Placki");
+        collection.add(1);
+
+        var iter = collection.getIterator();
+
+        assert.equal( iter.getNext(), 6, "Nie znaleziono wartości 6" );
+        assert.equal( iter.end(), false, "Elementy są w kolekcji a end zwraca co innego" );
+        assert.equal( iter.getNext(), "Placki", "Nie znalzeiono tekstu 'Placki'" );
+        assert.equal( iter.getNext(), 1, "Nie znaleziono wartości 1" );
+        assert.equal( iter.end(), true, "Nie ma więcej elementów w kolekcji, a end zwraca że są" );
+
+    });
+
+});
+
